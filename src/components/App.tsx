878de1aca4beb7e8e8e102acbb1a0baeabfa3f17
mac <mac@MacBook-Pro-de-mac.local>
2020-04-07 15:09:25 -0500

    adding the header component

--- src/components/App.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import '../assets/styles/App.scss';
-
-const App = () => {
-  return <h1>this is working</h1>;
-};
-
-export default App;
---
+++ src/containers/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Header from '../components/Header';
+import '../assets/styles/App.scss';
+
+const App = () => {
+    return <div className="App">
+        <Header />
+    </div>
+};
+
+export default App;
